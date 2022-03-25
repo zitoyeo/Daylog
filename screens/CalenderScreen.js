3e4ed2eb135b0a27449900d928703d29ce4d2aec
zitoyeo <wlsgh010203@gmail.com>
2022-03-25 15:28:14 +0900

    create Calendar

--- screens/CalenderScreen.js
+++ screens/CalenderScreen.js
@@ -1,9 +1,40 @@
-import React from "react";
+import { format } from "date-fns";
+import React, { useContext, useState, useMemo } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import LogContext from "../context/LogContext";
+import FeedList from "../components/FeedList";
+import CalendarView from "../components/CalendarView";
 
 function CalendarScreen() {
-  return <View style={styles.block} />;
+  const { logs } = useContext(LogContext);
+  const [selectedDate, setSelectedDate] = useState(
+    format(new Date(), "yyyy-mm-dd")
+  );
+
+  const markedDates = useMemo(() =>
+    logs.reduce((acc, current) => {
+      const formattedDate = format(new Date(current.date), "yyyy-mm-dd");
+      acc[formattedDate] = { marked: true };
+      return acc;
+    }, [])
+  );
+
+  const filteredLogs = logs.filter(
+    (log) => format(new Date(log.date), "yyyy-MM-dd") === selectedDate
+  );
+
+  return (
+    <FeedList
+      logs={filteredLogs}
+      ListHeaderComponent={
+        <CalendarView
+          markedDates={markedDates}
+          selectedDate={selectedDate}
+          onSelectDate={setSelectedDate}
+        />
+      }
+    />
+  );
 }
 
 const styles = StyleSheet.create({
